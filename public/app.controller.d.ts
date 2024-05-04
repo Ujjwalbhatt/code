fdd738e6142dd1f26def881b8d6fe5e342f03b50
Ujjwal Bhatt <ujjwalbhatt04@gmail.com>
2024-05-05 00:42:42 +0530

    master

--- public/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AppService } from './app.service';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
-    generateAIResponce(name: string, businessEmail: string, email: string, phone: string, companyName: string, companySize: string, ventureCapitalFunded: string, publicListedPostIPOCompany: string, projectType: string, tellUsAboutYourRequirement: string, requiredTechnicalExpertise: string): Promise<string>;
-}
--- public/app.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class AppService {
-    private genAI;
-    private model;
-    constructor();
-    getHello(): string;
-    generateAIResponce(prompt: string): Promise<string>;
-}
