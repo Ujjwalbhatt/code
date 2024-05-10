61efbe7b5d5049bc74b0ffad359bfe3d7e57c12a
Ujjwal Bhatt <ujjwalbhatt04@gmail.com>
2024-05-10 23:21:04 +0530

    master

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Enable CORS
+  app.enableCors({
+  origin: 'https://joinarena.webflow.io'
+});
   await app.listen(3000);
 }
 bootstrap();
