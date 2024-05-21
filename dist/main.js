eb7b608fa6cfba8a2a96664656947f6b9779cd90
Ujjwal Bhatt <ujjwalbhatt04@gmail.com>
2024-05-21 18:11:12 +0530

    master

--- dist/main.js
+++ dist/main.js
@@ -4,8 +4,19 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    app.enableCors();
-    app.enableCors({ origin: 'https://joinarena.ai' });
+    const allowedOrigins = ['https://www.joinarena.ai', 'https://joinarena.ai'];
+    app.enableCors({
+        origin: (origin, callback) => {
+            if (!origin)
+                return callback(null, true);
+            if (allowedOrigins.includes(origin)) {
+                return callback(null, true);
+            }
+            return callback(new Error('Not allowed by CORS'));
+        },
+        methods: 'GET,POST,PUT,DELETE',
+        allowedHeaders: 'Content-Type, Authorization',
+    });
     await app.listen(3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,23 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Enable CORS with specific origins
+  const allowedOrigins = ['https://www.joinarena.ai', 'https://joinarena.ai'];
 
-  // Enable CORS with additional settings
   app.enableCors({
-    origin: ['https://joinarena.ai','https://www.joinarena.ai'],
-
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);  // Allow requests with no origin, like mobile apps or curl requests
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+      return callback(new Error('Not allowed by CORS'));
+    },
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
 
   await app.listen(3000);
 }
+
 bootstrap();
