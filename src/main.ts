40514bc606c238ecf3bd49844ba05fe95e061a12
Ujjwal Bhatt <ujjwalbhatt04@gmail.com>
2024-05-21 17:59:38 +0530

    master

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS with additional settings
   app.enableCors({
-    origin: (origin, callback) => {
-      console.log("Incoming origin:", origin); // Debug log to see the incoming origin
-      if (!origin || /^https:\/\/(\w+\.)*joinarena.ai$/.test(origin)) {
-        console.log("Allowed by CORS"); // Debug log when origin is allowed
-        callback(null, true);
-      } else {
-        console.log("Blocked by CORS"); // Debug log when origin is blocked
-        callback(new Error('Not allowed by CORS'), false);
-      }
-    },
-    credentials: true
+    origin: ['https://joinarena.ai','https://www.joinarena.ai'],
+
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
   });
 
   await app.listen(3000);
 }
-
 bootstrap();
